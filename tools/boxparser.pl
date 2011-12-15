5a414b75efcd051559991710da4df52c066cf8b0
/emailAddress=andy.armstrong@bbc.co.uk/CN=Andy Armstrong/OU=BBC - FMT - Online Media Group/O=British Broadcasting Corporation/L=London/C=GB </emailAddress=andy.armstrong@bbc.co.uk/CN=Andy Armstrong/OU=BBC - FMT - Online Media Group/O=British Broadcasting Corporation/L=London/C=GB@6500d436-0a25-4ea1-a8e6-63f9231d3e1c>
2011-12-15 23:35:00 +0000

    Working bootstrap parser.
    git-svn-id: https://repo.dev.bbc.co.uk/playground/av2012/scratch/andy/hds_union@744758 6500d436-0a25-4ea1-a8e6-63f9231d3e1c

--- tools/boxparser.pl
+++ tools/boxparser.pl
@@ -5,6 +5,9 @@ package reader;
 use strict;
 use warnings;
 
+use Carp qw( croak );
+use Data::Dumper;
+
 sub new {
   my ( $class, $data, $pos, $len ) = @_;
   $pos = 0 unless defined $pos;
@@ -20,7 +23,7 @@ sub read {
   my ( $self, $count ) = @_;
   return $$self->{len} if $count < 0;
 
-  die "Truncated $self->{d} at offset $self->{p}"
+  croak "Truncated read at offset $self->{p}"
    if $self->{l} < $count;
 
   my $chunk = substr $self->{d}, $self->{p}, $count;
@@ -51,14 +54,23 @@ sub readZ {
   my $self = shift;
   my $tail = substr $self->{d}, $self->{p}, $self->{l};
   my $str  = ( $tail =~ /^(.*?)\0/ ) ? $1 : $tail;
-  $self->{p} += length $str + 1;
+  my $sz   = length( $str ) + 1;
+  $self->{p} += $sz;
+  $self->{l} -= $sz;
   return $str;
 }
 
-sub readZs {
-  my $self = shift;
-  [ map { $self->readZ } 1 .. $self->read8 ];
+sub read8ar {
+  my ( $self, $cb ) = @_;
+  [ map { $cb->( $self ) } 1 .. $self->read8 ];
 }
+
+sub read32ar {
+  my ( $self, $cb ) = @_;
+  [ map { $cb->( $self ) } 1 .. $self->read32 ];
+}
+
+sub readZs { shift->read8ar( \&readZ ) }
 
 package main;
 
@@ -72,8 +84,8 @@ use Path::Class;
 my $src = shift @ARGV;
 my $bs  = file( $src )->slurp;
 
-my @boxes = get_boxes( reader->new( $bs ) );
-print Dumper( \@boxes );
+my $boxes = get_boxes( reader->new( $bs ) );
+print Dumper( $boxes );
 
 sub get_box_info {
   my $rdr = shift;
@@ -107,7 +119,6 @@ sub get_boxes {
   my @boxes = ();
   while ( $rdr->avail ) {
     my $bi = get_box_info( $rdr );
-    #    print Dumper( $bi );
 
     if ( $bi->{type} eq 'abst' ) {
       push @boxes, get_bootstrap_box( $rdr, $bi );
@@ -123,13 +134,59 @@ sub get_boxes {
       $rdr->read( $bi->{size} - 8 );
     }
   }
+  return \@boxes;
+}
+
+sub expect_box {
+  my ( $rdr, $type ) = @_;
+  my $bi = get_box_info( $rdr );
+  die "Expected '$type', got '$bi->{type}'" unless $bi->{type} eq $type;
+  return get_full_box( $rdr, $bi );
+}
+
+sub get_segment_runs {
+  my $rdr = shift;
+  expect_box( $rdr, 'asrt' );
+  return {
+    quality => $rdr->readZs,
+    runs    => $rdr->read32ar(
+      sub {
+        my $rdr = shift;
+        {
+          first => $rdr->read32,
+          frags => $rdr->read32
+        };
+      }
+    ),
+  };
+}
+
+sub get_frag_duration_pair {
+  my $rdr = shift;
+  my $rec = {
+    first     => $rdr->read32,
+    timestamp => $rdr->read64,
+    duration  => $rdr->read32,
+  };
+  $rec->{discontinuity} = $rdr->read8 if $rec->{duration} == 0;
+  return $rec;
+}
+
+sub get_fragment_runs {
+  my $rdr = shift;
+  expect_box( $rdr, 'afrt' );
+  return {
+    timescale => $rdr->read32,
+    quality   => $rdr->readZs,
+    runs      => $rdr->read32ar( \&get_frag_duration_pair ),
+  };
 }
 
 sub get_bootstrap_box {
   my ( $rdr, $bi ) = @_;
-  my $fbi = get_full_box( $rdr, $bi );
 
   my %bs = (
+    bi                    => get_full_box( $rdr, $bi ),
     version               => $rdr->read32,
     flags                 => $rdr->read8,
     time_scale            => $rdr->read32,
@@ -140,21 +197,67 @@ sub get_bootstrap_box {
     quality               => $rdr->readZs,
     drm_data              => $rdr->readZ,
     metadata              => $rdr->readZ,
+    segment_run_tables    => $rdr->read8ar( \&get_segment_runs ),
+    fragment_run_tables   => $rdr->read8ar( \&get_fragment_runs ),
   );
 
   $bs{profile} = $bs{flags} >> 6;
   $bs{live}    = ( $bs{flags} & 0x20 ) ? 1 : 0;
   $bs{update}  = ( $bs{flags} & 0x01 ) ? 1 : 0;
 
-  print Dumper( { bi => $fbi, bs => \%bs } );
+  return \%bs;
 }
 
 sub get_frag_ra_box {
   my ( $rdr, $bi ) = @_;
+  my $fbi = get_full_box( $rdr, $bi );
+
+  my $sizes = $rdr->read8;
+
+  my $rd_id
+   = ( $sizes & 0x80 ) ? sub { $rdr->read32 } : sub { $rdr->read16 };
+  my $rd_ofs
+   = ( $sizes & 0x40 ) ? sub { $rdr->read64 } : sub { $rdr->read32 };
+
+  my %ra = (
+    bi         => $fbi,
+    sizes      => $sizes,
+    time_scale => $rdr->read32,
+    local      => $rdr->read32ar(
+      sub {
+        my $rdr = shift;
+        return {
+          time   => $rdr->read64,
+          offset => $rd_ofs->()
+        };
+      }
+    ),
+  );
+
+  if ( $sizes & 0x20 ) {
+    $ra{gloabls} = $rdr->read32ar(
+      sub {
+        my $rdr = shift;
+        return {
+          time             => $rdr->read64(),
+          segment          => $rd_id->(),
+          fragment         => $rd_id->(),
+          afra_offset      => $rd_ofs->(),
+          offset_from_afra => $rd_ofs->(),
+        };
+      }
+    );
+  }
+
+  return \%ra;
 }
 
 sub get_media_data_box {
   my ( $rdr, $bi ) = @_;
+  return {
+    bi   => $bi,
+    data => $rdr->read( $bi->{size} );
+  };
 }
 
 # vim:ts=2:sw=2:sts=2:et:ft=perl
