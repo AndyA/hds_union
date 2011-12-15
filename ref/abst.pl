f2c202935921ddeba931d32dbb6df0e77d66bc43
/emailAddress=andy.armstrong@bbc.co.uk/CN=Andy Armstrong/OU=BBC - FMT - Online Media Group/O=British Broadcasting Corporation/L=London/C=GB </emailAddress=andy.armstrong@bbc.co.uk/CN=Andy Armstrong/OU=BBC - FMT - Online Media Group/O=British Broadcasting Corporation/L=London/C=GB@6500d436-0a25-4ea1-a8e6-63f9231d3e1c>
2011-12-15 23:35:43 +0000

    What it looks like in Perl.
    git-svn-id: https://repo.dev.bbc.co.uk/playground/av2012/scratch/andy/hds_union@744766 6500d436-0a25-4ea1-a8e6-63f9231d3e1c

---
+++ ref/abst.pl
@@ -0,0 +1,114 @@
+$VAR1 = [
+  {
+    'profile'            => 0,
+    'drm_data'           => '',
+    'segment_run_tables' => [
+      {
+        'runs' => [
+          {
+            'first' => 1,
+            'frags' => 15
+          }
+        ],
+        'quality' => [],
+        'bi'      => {
+          'flags' => 0,
+          'ver'   => 0,
+          'type'  => 'asrt',
+          'size'  => 17
+        }
+      }
+    ],
+    'smpte_timecode_offset' => 0,
+    'metadata'              => '',
+    'bi'                    => {
+      'flags' => 0,
+      'ver'   => 0,
+      'type'  => 'abst',
+      'size'  => 274
+    },
+    'current_media_time'  => 60905,
+    'quality'             => [],
+    'flags'               => 0,
+    'version'             => 15,
+    'time_scale'          => 1000,
+    'live'                => 0,
+    'servers'             => [],
+    'update'              => 0,
+    'movie_identifier'    => '',
+    'fragment_run_tables' => [
+      {
+        'timescale' => 1000,
+        'runs'      => [
+          {
+            'first'     => 1,
+            'timestamp' => 0,
+            'duration'  => 4400
+          },
+          {
+            'first'     => 3,
+            'timestamp' => 8808,
+            'duration'  => 3300
+          },
+          {
+            'first'     => 4,
+            'timestamp' => 12112,
+            'duration'  => 4400
+          },
+          {
+            'first'     => 6,
+            'timestamp' => 20920,
+            'duration'  => 3300
+          },
+          {
+            'first'     => 7,
+            'timestamp' => 24224,
+            'duration'  => 4400
+          },
+          {
+            'first'     => 9,
+            'timestamp' => 33066,
+            'duration'  => 3200
+          },
+          {
+            'first'     => 10,
+            'timestamp' => 36302,
+            'duration'  => 4400
+          },
+          {
+            'first'     => 11,
+            'timestamp' => 40707,
+            'duration'  => 3800
+          },
+          {
+            'first'     => 12,
+            'timestamp' => 44511,
+            'duration'  => 4400
+          },
+          {
+            'first'     => 13,
+            'timestamp' => 48915,
+            'duration'  => 3300
+          },
+          {
+            'first'     => 14,
+            'timestamp' => 52218,
+            'duration'  => 4400
+          },
+          {
+            'first'     => 15,
+            'timestamp' => 56623,
+            'duration'  => 4300
+          }
+        ],
+        'quality' => [],
+        'bi'      => {
+          'flags' => 0,
+          'ver'   => 0,
+          'type'  => 'afrt',
+          'size'  => 205
+        }
+      }
+    ]
+  }
+];
---
+++ ref/afra.pl
@@ -0,0 +1,120 @@
+$VAR1 = [
+  {
+    'time_scale' => 1000,
+    'globals'    => [
+      {
+        'fragment'         => 1,
+        'time'             => 0,
+        'afra_offset'      => 663378,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 2,
+        'time'             => 4404,
+        'afra_offset'      => 4204379,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 3,
+        'time'             => 8808,
+        'afra_offset'      => 7527832,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 4,
+        'time'             => 12112,
+        'afra_offset'      => 9914593,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 5,
+        'time'             => 16516,
+        'afra_offset'      => 13642093,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 6,
+        'time'             => 20920,
+        'afra_offset'      => 16914747,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 7,
+        'time'             => 24224,
+        'afra_offset'      => 18970007,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 8,
+        'time'             => 28628,
+        'afra_offset'      => 23044620,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 9,
+        'time'             => 33066,
+        'afra_offset'      => 26887336,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 10,
+        'time'             => 36302,
+        'afra_offset'      => 29362083,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 11,
+        'time'             => 40707,
+        'afra_offset'      => 31707812,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 12,
+        'time'             => 44511,
+        'afra_offset'      => 35157965,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 13,
+        'time'             => 48915,
+        'afra_offset'      => 38464620,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 14,
+        'time'             => 52218,
+        'afra_offset'      => 40967843,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      },
+      {
+        'fragment'         => 15,
+        'time'             => 56623,
+        'afra_offset'      => 44960711,
+        'segment'          => 1,
+        'offset_from_afra' => 0
+      }
+    ],
+    'local' => [],
+    'sizes' => 224,
+    'bi'    => {
+      'flags' => 0,
+      'ver'   => 0,
+      'type'  => 'afra',
+      'size'  => 497
+    }
+  }
+];
