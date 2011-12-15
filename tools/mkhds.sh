471070a3db6a6e7ba5e4a4ee74798d5fbfa74c3d
/emailAddress=andy.armstrong@bbc.co.uk/CN=Andy Armstrong/OU=BBC - FMT - Online Media Group/O=British Broadcasting Corporation/L=London/C=GB </emailAddress=andy.armstrong@bbc.co.uk/CN=Andy Armstrong/OU=BBC - FMT - Online Media Group/O=British Broadcasting Corporation/L=London/C=GB@6500d436-0a25-4ea1-a8e6-63f9231d3e1c>
2011-12-15 23:34:24 +0000

    Package HDS.
    git-svn-id: https://repo.dev.bbc.co.uk/playground/av2012/scratch/andy/hds_union@744750 6500d436-0a25-4ea1-a8e6-63f9231d3e1c

--- tools/mkhds.sh
+++ tools/mkhds.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-outdir="media/hds"
-indir="media/src"
 
-mkdir -p "$outdir"
+cd "$( dirname "$0" )"
+f4fpackager="$PWD/../f4fpackager/linux/f4fpackager"
+cd "../media/src"
+
+prevf4m=
 for br in {1..6}; do
-  src="$indir/bbc1_hd_p${br}.mp4"
+  base="bbc1_hd_p${br}"
+  src="$base.mp4"
+  f4m="$base.f4m"
+  br=$( mediainfo -f "$src" | \
+    perl -ne 'print int($1/1000) if /^overall\s+bit\s+rate\s*:\s*(\d+)\s*$/i' )
+  $f4fpackager  \
+    --input-file=$src \
+    --bitrate=$br \
+    $prevf4m
+  prevf4m="--manifest-file=$f4m"
 done
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
