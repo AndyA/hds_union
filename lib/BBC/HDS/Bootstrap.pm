daba06bb37a533e274b56a1b47de96410b696d17
/emailAddress=andy.armstrong@bbc.co.uk/CN=Andy Armstrong/OU=BBC - FMT - Online Media Group/O=British Broadcasting Corporation/L=London/C=GB </emailAddress=andy.armstrong@bbc.co.uk/CN=Andy Armstrong/OU=BBC - FMT - Online Media Group/O=British Broadcasting Corporation/L=London/C=GB@6500d436-0a25-4ea1-a8e6-63f9231d3e1c>
2011-12-16 13:08:24 +0000

    Build atom index.
    git-svn-id: https://repo.dev.bbc.co.uk/playground/av2012/scratch/andy/hds_union@745208 6500d436-0a25-4ea1-a8e6-63f9231d3e1c

--- lib/BBC/HDS/Bootstrap.pm
+++ lib/BBC/HDS/Bootstrap.pm
@@ -16,6 +16,21 @@ sub new {
   return bless {@_}, $class;
 }
 
+sub _build_index {
+  my $self = shift;
+  my %idx  = ();
+  for my $atom ( @{ $self->bs } ) {
+    my $type = $atom->{bi}{type};
+    push @{ $idx{$type} }, $atom;
+  }
+  return \%idx;
+}
+
+sub index {
+  my $self = shift;
+  $self->{index} ||= $self->_build_index;
+}
+
 1;
 
 # vim:ts=2:sw=2:sts=2:et:ft=perl
